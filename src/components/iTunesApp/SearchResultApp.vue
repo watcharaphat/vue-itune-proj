d31d55e727aff01223ba2a1e1621361cfe4756b2
watcharaphat <watcharaphat.3166@gmail.com>
2017-06-07 14:49:51 +0700

    edit the result line

--- src/components/iTunesApp/SearchResultApp.vue
+++ src/components/iTunesApp/SearchResultApp.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     <div v-if="results">
-      <div class="result-textbox">
-        <div class="column is-8">
-          <div class="found-textbox">Found: {{ results.length }} Results</div>
+      <div class="columns">
+        <div class="result-textbox">
+          <div class="column is-8">
+            <div class="found-textbox">Found: {{ results.length }} Results</div>
+          </div>
         </div>
       </div>
       <searchresult v-for="result in results" :key="result.trackId" v-bind:result="result"></searchresult>
